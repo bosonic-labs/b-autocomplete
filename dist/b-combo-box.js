d790343514f65e20592bf9f3c8f3281dee81bc31
Raphaël Rougeron <goldoraf@gmail.com>
2014-02-21 16:20:55 +0100

    We can now pick suggestions

--- dist/b-combo-box.js
+++ dist/b-combo-box.js
@@ -8,6 +8,12 @@
     get suggestionList() {
         return this.querySelector('ul');
     },
+    get selectable() {
+        return this.querySelector('b-selectable');
+    },
+    get input() {
+        return this.querySelector('input[type=text]');
+    },
     readyCallback: function () {
         this.listChanged(this.getAttribute('list'));
         this.appendChild(this.template.content.cloneNode(true));
@@ -34,16 +40,31 @@
             li.innerHTML = option.text || option.value;
             list.appendChild(li);
         });
+        if (this.options.length > 0) {
+            this.selectable.setAttribute('selected', 0);
+        }
     },
     toggleSuggestionList: function () {
         this.suggestionList.hasAttribute('visible') ? this.hideSuggestionList() : this.showSuggestionList();
     },
     showSuggestionList: function () {
         this.paintSuggestionList();
+        this.attachListEvents();
         this.suggestionList.setAttribute('visible', '');
+        this.selectable.focus();
     },
     hideSuggestionList: function () {
         this.suggestionList.removeAttribute('visible');
+    },
+    attachListEvents: function () {
+        this.selectable.addEventListener('b-activate', this.pickSuggestion.bind(this), false);
+    },
+    pickSuggestion: function (e) {
+        this.input.value = this.getItemValue(e.detail.item);
+        this.hideSuggestionList();
+    },
+    getItemValue: function (itemIndex) {
+        return this.querySelectorAll('li')[itemIndex].innerHTML;
     },
     template: '        <input type="text" value="">        <a class="b-combo-box-toggle">show</a>        <b-selectable target="li">            <ul></ul>        </b-selectable>    '
 }
