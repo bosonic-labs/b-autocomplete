392dee557d25fba2d410a8d2c6c05b7d020b7b5b
blandine <bdescamps@viareport.com>
2014-03-10 11:55:50 +0100

    add a test on toggle event

--- test/b-combo-box.js
+++ test/b-combo-box.js
@@ -1,5 +1,26 @@
-describe("b-combo-box", function() {
-    it("should XXX", function() {
+var mouse = effroi.mouse;
 
+function createCombo() {
+    var combo = document.createElement('b-combo-box');
+    var datalist = document.createElement('b-datalist');
+    datalist.setAttribute('id', 'data');
+    combo.setAttribute('list', 'data');
+    datalist.innerHTML='<option value="first"></option><option value="second"></option>';
+    document.body.appendChild(combo);
+    document.body.appendChild(datalist);
+
+    return combo;
+}
+
+describe("b-combo-box", function() {
+    describe("when cliking on b-combo box", function() {
+        it("should toggle suggestionList", function() {
+            var combo = createCombo();
+
+            mouse.click(combo.querySelector('.b-combo-box-toggle'));
+
+            var isVisible = combo.suggestionList.hasAttribute('visible');
+            assert.equal(isVisible, true);
+        });
     });
 });
